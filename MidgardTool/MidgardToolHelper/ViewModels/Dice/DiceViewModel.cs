95eae7a29e32955c178abb0ebdd30ea280df2f9b
SebastianFlosbach <sebflos@online.de>
2019-10-07 09:25:34 +0200

    Updated project

---
+++ MidgardTool/MidgardToolHelper/ViewModels/Dice/DiceViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidgardToolHelper.ViewModels.Dice
+{
+    public class DiceViewModel : BaseViewModell
+    {
+        #region Window Properties
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Construction
+
+        #endregion
+    }
+}
